095cfc0014fc6bac32d4555c789d483cad7ccd03
hakon.isaksen <hakon.isaksen@gmail.com>
2023-05-01 00:17:50 +0200

    functionality for showing DB on frontend soon finished

--- Fullstack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ Fullstack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -8,38 +9,19 @@ import { Employee } from 'src/app/models/employee.model';
 })
 export class EmployeesListComponent implements OnInit {
 
-  employees1: Employee [] = [];
-  employees: Employee[] = [
-    {
-      id: 'u3h4js74',
-      name: 'Mat Clark',
-      email: 'mat.clark@gmail.com',
-      phone: 433943849,
-      salary: 40000,
-      department: 'Cyber Secuirty'
-    },
-    {
-      id: 'j3hs8fk5',
-      name: 'Paul Johnsen',
-      email: 'pauljohnsen@live.com',
-      phone: 995748234,
-      salary: 50000,
-      department: 'IT'
-    },
-    {
-      id: '92jd7fh4d',
-      name: 'Andrew Clint',
-      email: 'andrew_clint@outlook.com',
-      phone: 738473623,
-      salary: 35000,
-      department: 'HR'
-    },
-  ];
-  constructor() {}
+  employees: Employee [] = [];
+  constructor(private employeesService: EmployeesService) { }
 
   ngOnInit(): void {
-     
-
+     this.employeesService.getAllEmployees()
+     .subscribe({
+        next: (employees) => {
+          this.employees = employees;
+      },
+      error: (response) => {
+        console.log(response);
+      }
+     })
     }
   }
 
---
+++ Fullstack UI/FullStack.UI/src/app/services/employees.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/enviroments/enviroments';
+import { Employee } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor(private http: HttpClient) { }
+
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.baseApiUrl + '/api/Employees');
+
+}
+
+}
