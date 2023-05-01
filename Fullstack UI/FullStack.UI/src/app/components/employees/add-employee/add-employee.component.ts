5b899b32d04ea8fc54b79c6d538650c3fcb768ae
hakon.isaksen <hakon.isaksen@gmail.com>
2023-05-01 18:45:56 +0200

    Added new page Add employee with bootstrap form and assigned the inputs

---
+++ Fullstack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit{
+  
+  
+  
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
